857fa26a7483d50bffdfb2dcd92bbff59d31fa10
eitah <eli@spantree.net>
2021-04-23 10:48:01 -0500

    Comment out yml handler as  instructed

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -13,24 +13,28 @@ func main() {
 	// Build the map handler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
-		"yaml-godoc": "https://godoc.org/gopkg.in/yaml.v2",
+		"yaml-godoc":      "https://godoc.org/gopkg.in/yaml.v2",
+		"dogs":            "https://www.somesite.com/a-story-about-dogs",
 	}
-	mapHandler := urlshort.MapHandler(pathsToUrls,  mux)
+	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
 	// build the yaml handler using the mapHandler as a fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
-	if err != nil {
-		panic(err)
-	}
+	// 	yaml := `
+	// - path: /urlshort
+	//   url: https://github.com/gophercises/urlshort
+	// - path: /urlshort-final
+	//   url: https://github.com/gophercises/urlshort/tree/solution
+	// - path: /dogs
+	//   url: https://www.somesite.com/a-story-about-dogs
+	// `
+	// 	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 
 	fmt.Println("starting the server on :8080")
-	http.ListenAndServe(":8080", yamlHandler)
+	http.ListenAndServe(":8080", mapHandler)
+	// http.ListenAndServe(":8080", yamlHandler)
 }
 
 func defaultMux() *http.ServeMux {
